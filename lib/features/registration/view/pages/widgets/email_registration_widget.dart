fa5e073348193338dfbb0aa0ba7cd65f42654c21
sb-dor <avaz.shams.2002@gmail.com>
2024-11-05 09:07:22 +0500

    solved problem of registration widget. Width and height of widget

--- lib/features/registration/view/pages/widgets/email_registration_widget.dart
+++ lib/features/registration/view/pages/widgets/email_registration_widget.dart
@@ -60,156 +60,159 @@ class _EmailRegistrationWidgetState extends ConsumerState<EmailRegistrationWidge
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        FocusableActionDetector(
-          mouseCursor: SystemMouseCursors.click,
-          onShowHoverHighlight: (hovered) {
-            if (hovered) {
-              setState(() {
-                _closeButton = true;
-              });
-            } else {
-              setState(() {
-                _closeButton = false;
-              });
-            }
-          },
-          child: GestureDetector(
-            onTap: () {
-              ref.read(registrationProviderProvider.notifier).changeStateInEmailRegistration(
-                    LetsGetStartedState.showRegistrationButtons,
-                  );
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FocusableActionDetector(
+            mouseCursor: SystemMouseCursors.click,
+            onShowHoverHighlight: (hovered) {
+              if (hovered) {
+                setState(() {
+                  _closeButton = true;
+                });
+              } else {
+                setState(() {
+                  _closeButton = false;
+                });
+              }
             },
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 100),
-              width: 30,
-              height: 30,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  border: Border.all(
-                    color: _closeButton ? Colors.white : Colors.grey,
-                  ),
-                  color: Colors.transparent),
-              child: Icon(
-                Icons.close,
-                size: 20,
-                color: _closeButton ? Colors.white : Colors.grey,
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.only(left: 100 / 2.85, right: 100 / 2.85),
-          child: Text(
-            "Enter your email address",
-            style: GoogleFonts.inter(
-              fontSize: 40,
-              color: Colors.white,
-              height: 1,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        const SizedBox(height: 30),
-        Container(
-          width: 320,
-          height: 40,
-          margin: const EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.white.withOpacity(0.4),
-            ),
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: Stack(
-            children: [
-              Positioned.fill(
-                child: Center(
-                  child: TextField(
-                    style: GoogleFonts.inter(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 13,
+            child: GestureDetector(
+              onTap: () {
+                ref.read(registrationProviderProvider.notifier).changeStateInEmailRegistration(
+                      LetsGetStartedState.showRegistrationButtons,
+                    );
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 100),
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                      color: _closeButton ? Colors.white : Colors.grey,
                     ),
-                    cursorColor: Colors.white,
-                    controller: _emailController,
-                    onTapOutside: (v) {
-                      _focusNode.requestFocus();
-                    },
-                    onSubmitted: (v) {
-                      if (v.isEmpty) return;
-                      ref.read(registrationProviderProvider.notifier).emailLink(
-                            getIt<RegistrationRepo>(),
-                            authData: AuthData(
-                              email: v.trim(),
-                            ),
-                          );
-                      onSubmit();
-                    },
-                    decoration: InputDecoration(
-                      isDense: true,
-                      contentPadding: const EdgeInsets.only(left: 10),
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: "Type your Email",
-                      hintStyle: GoogleFonts.inter(
-                        color: Colors.grey,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
+                    color: Colors.transparent),
+                child: Icon(
+                  Icons.close,
+                  size: 20,
+                  color: _closeButton ? Colors.white : Colors.grey,
                 ),
               ),
-              Positioned(
-                bottom: 0,
-                top: 0,
-                right: 0,
-                child: FocusableActionDetector(
-                  focusNode: _focusNode,
-                  mouseCursor: SystemMouseCursors.click,
-                  onShowHoverHighlight: (hovered) {
-                    if (hovered) {
-                      setState(() {
-                        _sendLinkHover = true;
-                      });
-                    } else {
-                      setState(() {
-                        _sendLinkHover = false;
-                      });
-                    }
-                  },
-                  shortcuts: enterShortcut.shortCuts ?? {},
-                  actions: enterShortcut.actions ?? {},
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 100),
-                    margin: const EdgeInsets.only(top: 2, bottom: 2, right: 2),
-                    width: 90,
-                    decoration: BoxDecoration(
-                      color: _sendLinkHover ? Colors.white : Colors.white.withOpacity(0.3),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Send link",
-                        style: GoogleFonts.inter(
-                          fontWeight: FontWeight.w500,
-                          color: _sendLinkHover ? Colors.black : Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 100 / 2.85, right: 100 / 2.85),
+            child: Text(
+              "Enter your email address",
+              style: GoogleFonts.inter(
+                fontSize: 40,
+                color: Colors.white,
+                height: 1,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Container(
+            width: 320,
+            height: 40,
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white.withOpacity(0.4),
+              ),
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Center(
+                    child: TextField(
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13,
+                      ),
+                      cursorColor: Colors.white,
+                      controller: _emailController,
+                      onTapOutside: (v) {
+                        _focusNode.requestFocus();
+                      },
+                      onSubmitted: (v) {
+                        if (v.isEmpty) return;
+                        ref.read(registrationProviderProvider.notifier).emailLink(
+                              getIt<RegistrationRepo>(),
+                              authData: AuthData(
+                                email: v.trim(),
+                              ),
+                            );
+                        onSubmit();
+                      },
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: const EdgeInsets.only(left: 10),
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        hintText: "Type your Email",
+                        hintStyle: GoogleFonts.inter(
+                          color: Colors.grey,
+                          fontSize: 14,
                         ),
                       ),
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
-        )
-      ],
+                Positioned(
+                  bottom: 0,
+                  top: 0,
+                  right: 0,
+                  child: FocusableActionDetector(
+                    focusNode: _focusNode,
+                    mouseCursor: SystemMouseCursors.click,
+                    onShowHoverHighlight: (hovered) {
+                      if (hovered) {
+                        setState(() {
+                          _sendLinkHover = true;
+                        });
+                      } else {
+                        setState(() {
+                          _sendLinkHover = false;
+                        });
+                      }
+                    },
+                    shortcuts: enterShortcut.shortCuts ?? {},
+                    actions: enterShortcut.actions ?? {},
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 100),
+                      margin: const EdgeInsets.only(top: 2, bottom: 2, right: 2),
+                      width: 90,
+                      decoration: BoxDecoration(
+                        color: _sendLinkHover ? Colors.white : Colors.white.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Send link",
+                          style: GoogleFonts.inter(
+                            fontWeight: FontWeight.w500,
+                            color: _sendLinkHover ? Colors.black : Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/features/registration/view/pages/widgets/lets_get_started_registration_widget.dart
+++ lib/features/registration/view/pages/widgets/lets_get_started_registration_widget.dart
@@ -23,164 +23,167 @@ class _LetsGetStartedRegistrationWidgetState
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 100 / 2.85, right: 100 / 2.85),
-          child: Text(
-            "Let's get started",
-            style: GoogleFonts.inter(
-              fontSize: 40,
-              color: Colors.white,
-              height: 1,
-              fontWeight: FontWeight.bold,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 100 / 2.85, right: 100 / 2.85),
+            child: Text(
+              "Let's get started",
+              style: GoogleFonts.inter(
+                fontSize: 40,
+                color: Colors.white,
+                height: 1,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
           ),
-        ),
-        const SizedBox(height: 10),
-        Padding(
-          padding: const EdgeInsets.only(left: 100 / 2.85, right: 100 / 2.85),
-          child: Text(
-            "Bring your personal email, connect your work later",
-            style: GoogleFonts.inter(
-              fontSize: 16,
-              color: Colors.white54,
-              height: 1.3,
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 100 / 2.85, right: 100 / 2.85),
+            child: Text(
+              "Bring your personal email, connect your work later",
+              style: GoogleFonts.inter(
+                fontSize: 16,
+                color: Colors.white54,
+                height: 1.3,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
           ),
-        ),
-        const SizedBox(height: 30),
-        SizedBox(
-          width: 250,
-          child: ElevatedButton(
-            onPressed: () async {
-              await ref.read(registrationProviderProvider.notifier).googleAuth(
-                    getIt<RegistrationRepo>(),
-                  );
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const FaIcon(
-                  FontAwesomeIcons.google,
-                  color: Colors.black,
-                  size: 20,
-                ),
-                const SizedBox(width: 10),
-                Flexible(
-                  child: Text(
-                    "Continue with Google",
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                    ),
-                    textAlign: TextAlign.center,
+          const SizedBox(height: 30),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              onPressed: () async {
+                await ref.read(registrationProviderProvider.notifier).googleAuth(
+                      getIt<RegistrationRepo>(),
+                    );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const FaIcon(
+                    FontAwesomeIcons.google,
+                    color: Colors.black,
+                    size: 20,
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        const SizedBox(height: 15),
-        SizedBox(
-          width: 250,
-          child: ElevatedButton(
-            clipBehavior: Clip.none,
-            onPressed: () async {
-              await ref.read(registrationProviderProvider.notifier).facebookAuth(
-                    getIt<RegistrationRepo>(),
-                  );
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const FaIcon(
-                  FontAwesomeIcons.facebook,
-                  color: Colors.black,
-                  size: 20,
-                ),
-                const SizedBox(width: 10),
-                Flexible(
-                  child: Text(
-                    "Continue with Facebook",
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
+                  const SizedBox(width: 10),
+                  Flexible(
+                    child: Text(
+                      "Continue with Google",
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 15),
-        FocusableActionDetector(
-          mouseCursor: SystemMouseCursors.click,
-          onShowHoverHighlight: (hovered) {
-            if (hovered) {
-              setState(() {
-                _onShowEmailRegistrationHover = false;
-              });
-            } else {
-              setState(() {
-                _onShowEmailRegistrationHover = true;
-              });
-            }
-          },
-          child: GestureDetector(
-            onTap: () {
-              ref.read(registrationProviderProvider.notifier).changeStateInEmailRegistration(
-                    LetsGetStartedState.showEmailRegistration,
-                  );
+          const SizedBox(height: 15),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              clipBehavior: Clip.none,
+              onPressed: () async {
+                await ref.read(registrationProviderProvider.notifier).facebookAuth(
+                      getIt<RegistrationRepo>(),
+                    );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const FaIcon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.black,
+                    size: 20,
+                  ),
+                  const SizedBox(width: 10),
+                  Flexible(
+                    child: Text(
+                      "Continue with Facebook",
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          FocusableActionDetector(
+            mouseCursor: SystemMouseCursors.click,
+            onShowHoverHighlight: (hovered) {
+              if (hovered) {
+                setState(() {
+                  _onShowEmailRegistrationHover = false;
+                });
+              } else {
+                setState(() {
+                  _onShowEmailRegistrationHover = true;
+                });
+              }
             },
-            child: SizedBox(
-              width: 250,
-              height: 30,
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 150),
+            child: GestureDetector(
+              onTap: () {
+                ref.read(registrationProviderProvider.notifier).changeStateInEmailRegistration(
+                      LetsGetStartedState.showEmailRegistration,
+                    );
+              },
+              child: SizedBox(
                 width: 250,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color:
-                      _onShowEmailRegistrationHover ? Colors.white : Colors.white.withOpacity(0.4),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    FaIcon(
-                      Icons.mail_outline,
-                      color: _onShowEmailRegistrationHover ? Colors.black : Colors.white,
-                      size: 20,
-                    ),
-                    const SizedBox(width: 10),
-                    Flexible(
-                      child: GestureDetector(
-                        onTap: () {
-                          ref
-                              .read(registrationProviderProvider.notifier)
-                              .changeStateInEmailRegistration(
-                                LetsGetStartedState.showEmailRegistration,
-                              );
-                        },
-                        child: Text(
-                          "Continue with Email",
-                          style: GoogleFonts.inter(
-                            color: _onShowEmailRegistrationHover ? Colors.black : Colors.white,
+                height: 30,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 150),
+                  width: 250,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color:
+                        _onShowEmailRegistrationHover ? Colors.white : Colors.white.withOpacity(0.4),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FaIcon(
+                        Icons.mail_outline,
+                        color: _onShowEmailRegistrationHover ? Colors.black : Colors.white,
+                        size: 20,
+                      ),
+                      const SizedBox(width: 10),
+                      Flexible(
+                        child: GestureDetector(
+                          onTap: () {
+                            ref
+                                .read(registrationProviderProvider.notifier)
+                                .changeStateInEmailRegistration(
+                                  LetsGetStartedState.showEmailRegistration,
+                                );
+                          },
+                          child: Text(
+                            "Continue with Email",
+                            style: GoogleFonts.inter(
+                              color: _onShowEmailRegistrationHover ? Colors.black : Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
